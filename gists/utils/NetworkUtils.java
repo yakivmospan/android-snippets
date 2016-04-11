eae16a6b0754387f570c41edfe330eecc0193b24
Yakiv Mospan <yakiv.mospan@gmail.com>
2016-04-11 14:57:49 +0300

    Create NetworkUtils.java

---
+++ gists/utils/NetworkUtils.java
@@ -0,0 +1,49 @@
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.annotation.NonNull;
+
+
+public class NetworkUtils {
+
+    public static boolean isConnected(@NonNull Context context) {
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    public static boolean isWifiConnected(@NonNull Context context) {
+        return isConnected(context, ConnectivityManager.TYPE_WIFI);
+    }
+
+    public static boolean isMobileConnected(@NonNull Context context) {
+        return isConnected(context, ConnectivityManager.TYPE_MOBILE);
+    }
+
+    private static boolean isConnected(@NonNull Context context, int type) {
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            NetworkInfo networkInfo = connMgr.getNetworkInfo(type);
+            return networkInfo != null && networkInfo.isConnected();
+        } else {
+            return isConnected(connMgr, type);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private static boolean isConnected(@NonNull ConnectivityManager connMgr, int type) {
+        Network[] networks = connMgr.getAllNetworks();
+        NetworkInfo networkInfo;
+        for (Network mNetwork : networks) {
+            networkInfo = connMgr.getNetworkInfo(mNetwork);
+            if (networkInfo != null && networkInfo.getType() == type && networkInfo.isConnected()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
